4e22e4b113ad4b2e279f45a9a26ae773d1676f7a
Chandu Imasha <chandu.2019289@iit.ac.lk>
2024-10-09 15:48:43 +0530

    Created the resume page

--- src/containers/resume/index.jsx
+++ src/containers/resume/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { BsInfoCircleFill } from "react-icons/bs";
 import PageHeaderContent from "../../components/pageHeaderContent";
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
+import { data } from "./utils";
+import 'react-vertical-timeline-component/style.min.css'
+import './styles.scss';
+import { MdWork } from "react-icons/md";
+
 
 const Resume = () => {
   return (
@@ -9,6 +15,94 @@ const Resume = () => {
         headerText="My Resume"
         icon={<BsInfoCircleFill size={40} />}
       />
+      <div className="timeline">
+        <div className="timeline_experience">
+          <h3 className="timeline_experience_header-text">Experience</h3>
+          <VerticalTimeline
+          layout={'1-column'}
+          lineColor="var(--yellow-theme-main-color)"
+          >
+            {
+              data.experience.map((item,i)=>(
+                <VerticalTimelineElement
+                  key={i}
+                  className="timeline_experience_vertical-timeline-element"
+                  contentStyle={{
+                    background: 'none',
+                    color: 'var(--yellow-theme-sub-text-color)',
+                    border : '1.5px solid var(--yellow-theme-main-color)'
+
+                  }}
+                  // date="2020 - Present"
+                  icon={<MdWork/>}
+                  iconStyle={{
+                    background: '#181818',
+                    color: 'var(--yellow-theme-main-color)',
+                  }}
+                >
+                  <div className="vertical-timeline-element-title-wrapper">
+                    <h3>
+                      {item.title}
+                    </h3>
+
+                    <h4>
+                      {item.subTitle}
+                    </h4>
+                    
+                  </div>
+                  <p className="vertical-timeline-element-title-wrapper-description">{item.description}</p>
+                  <p className="vertical-timeline-element-title-wrapper-date">{item.date}</p>
+
+                </VerticalTimelineElement>
+              ))
+            }
+
+          </VerticalTimeline>
+        </div>
+        <div className="timeline_education">
+        <h3 className="timeline_education_header-text">Education</h3>
+        <VerticalTimeline
+          layout={'1-column'}
+          lineColor="var(-yellow-theme-main-color)"
+          >
+        {
+              data.education.map((item,i)=>(
+                <VerticalTimelineElement
+                  key={i}
+                  className="timeline_experience_vertical-timeline-element"
+                  contentStyle={{
+                    background: 'none',
+                    color: 'var(--yellow-theme-sub-text-color)',
+                    border : '1.5px solid var(--yellow-theme-main-color)'
+
+                  }}
+                  // date="2020 - Present"
+                  icon={<MdWork/>}
+                  iconStyle={{
+                    background: '#181818',
+                    color: 'var(--yellow-theme-main-color)',
+                  }}
+                >
+                  <div className="vertical-timeline-element-title-wrapper">
+                    <h3>
+                      {item.title}
+                    </h3>
+
+                    <h4>
+                      {item.subTitle}
+                    </h4>
+                  </div>
+                  <p className="vertical-timeline-element-title-wrapper-description">{item.description}</p>
+                  <p className="vertical-timeline-element-title-wrapper-date">{item.date}</p>
+
+
+                </VerticalTimelineElement>
+              ))
+            }
+            </VerticalTimeline>
+        </div>
+
+      </div>
     </section>
   );
 };
---
+++ src/containers/resume/utils.js
@@ -0,0 +1,42 @@
+export const data = {
+    experience: [
+        {
+            title: "System Support Engineer -",
+            subTitle: "Green Way Asia Lanka (Pvt) Ltd",
+            description: "Focused on supporting and maintaining an ERP system, collaborating with cross-functional teams to gather requirements, troubleshoot issues, and optimize system performance. Demonstrated strong problem-solving skills and a proactive approach to resolving technical challenges, while staying updated on emerging technologies to drive efficiency and innovation.",
+            date: "2023 – 2024"
+        },
+        {
+            title: "Intern-Software Engineer -",
+            subTitle: "Intahub Private Limited",
+            description: "Focused on developing and maintaining frontend systems for inventory management, POS, banquet management, and room reservation solutions. Responsibilities included implementing features, conducting code reviews, debugging, and collaborating with cross-functional teams to ensure seamless project execution. Contributed to quality assurance efforts, researched new technologies, and provided technical documentation to enhance development processes and system performance.",
+            date: "2021 – 2022"
+        },
+    ],
+    education: [
+        {
+            title: "BEng (Hons) Software Engineering -",
+            subTitle: "Informatics Institute of Technology (IIT)",
+            description: "Achieved a 2nd Class Upper Division, demonstrating strong academic performance and a commitment to excellence throughout the degree program.",
+            date: "2019 – 2023"
+        },
+        {
+            title: "B.Sc. in Physical Science - ICT -",
+            subTitle: "University of Kelaniya",
+            description: "Had to discontinue the government degree to focus on a private degree",
+            date: "2021 – 2022"
+        },
+        {
+            title: "GCE Advanced Level Examination -",
+            subTitle: "D.S. Senanayake College, Colombo",
+            description: "ICT - B, Combined Mathematics - C, Physics - C, General English - C",
+            date: "2017 – 2019"
+        },
+        {
+            title: "GCE Ordinary Level Examination -",
+            subTitle: "D.S. Senanayake College, Colombo",
+            description: "Mathematics - A, English - A, History - A, Buddhism - A, Sinhala - A, Art - A, Health & Physical Education - A, Business & Accounting Studies - A, Science - B",
+            date: "2011 – 2016"
+        },
+    ]
+}
